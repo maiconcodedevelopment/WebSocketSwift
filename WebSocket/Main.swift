f44ca82849d41d4f814ae335b9b571d370c6e14e
Maicon Douglas Gonçalves Fazzio <maicon.fazzio@actdigital.com.br>
2022-06-13 20:23:30 -0300

    teste

---
+++ WebSocket/Main.swift
@@ -0,0 +1,14 @@
+//
+//  Main.swift
+//  WebSocket
+//
+//  Created by ACT on 13/06/22.
+//
+
+import Foundation
+
+public struct WebSocketApplication{
+    static func test(){
+        print("yes test")
+    }
+}
---
+++ WebSocket/WebSocket.h
@@ -0,0 +1,18 @@
+//
+//  WebSocket.h
+//  WebSocket
+//
+//  Created by ACT on 13/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WebSocket.
+FOUNDATION_EXPORT double WebSocketVersionNumber;
+
+//! Project version string for WebSocket.
+FOUNDATION_EXPORT const unsigned char WebSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebSocket/PublicHeader.h>
+
+
